02585ad2a421bd6bf44ca3e416770fe22d22bf4e
carloscncdev <carloscndev@gmail.com>
2026-06-24 13:29:30 -0600

    feat: update timelime progress bar'

--- src/components/Timeline/index.tsx
+++ src/components/Timeline/index.tsx
@@ -2,12 +2,23 @@ import styles from './Timeline.module.css'
 
 interface Props {
   frameId: number
+   totalFrames?: number
 }
 
-export const Timeline = ({ frameId }: Props) => {
+export const Timeline = ({ frameId,  totalFrames = 1 }: Props) => {
+  const pct = totalFrames > 0 ? ((frameId + 1) / totalFrames) * 100 : 0
+  
   return (
     <div className={styles.timeline}>
-      Frame: {frameId}
+      <span> Frame: {frameId}</span>
+      <div className={styles.progressBarButtonsContainer}>
+        <button className={styles.progressBarButton}>&lt;</button>
+        <span>Frame {frameId + 1} / {totalFrames}</span>
+        <button className={styles.progressBarButton}>&gt;</button>
+      </div>
+      <div className={styles.progressBarContainer}>
+        <div className={styles.progressBar} style={{ width: `${pct}%` }}/>
+      </div>
     </div>
   )
 }
