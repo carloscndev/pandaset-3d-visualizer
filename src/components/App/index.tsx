f68fa271329281156786fc0eb5b78c0516de6008
carloscncdev <carloscndev@gmail.com>
2026-06-23 23:43:59 -0600

    feat: fetch data frame_00

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import Viewer3D from '../Viewer3D'
 import styles from './App.module.css'
 
 
-const App = () => (
-  <div id="3d-viewer-container" className={styles.container}>
+const App = () => {
+  const [data, setData] = useState<any>();
+
+  useEffect(() => {
+    const loadData = async() => {
+      try {
+        const response = await fetch('https://static.scale.com/uploads/pandaset-challenge/frame_00.json');
+        const json = await response.json();
+        console.log(json);
+        setData(json);
+      } catch(e) {
+        console.log(e)
+      }
+    }
+
+    loadData();
+  }, []);
+
+  return (
+   <div id="3d-viewer-container" className={styles.container}>
     <Viewer3D />
   </div>
-);
+)};
 
 export default App;
