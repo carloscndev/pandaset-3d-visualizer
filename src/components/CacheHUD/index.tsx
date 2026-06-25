7aa2088fe287a414c973de30d4dd6e6d16fc7530
carloscncdev <carloscndev@gmail.com>
2026-06-24 22:54:16 -0600

    fix: coding style

--- src/components/CacheHUD/index.tsx
+++ src/components/CacheHUD/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { FrameCache } from '../../utils/FrameCache';
 import styles from './CacheHUD.module.css';
 
--- src/components/InfoCuboidLabel/index.tsx
+++ src/components/InfoCuboidLabel/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import styles from './InfoCuboidLabel.module.css';
+
 import { Cuboid } from '../../types';
+import styles from './InfoCuboidLabel.module.css';
 
 interface Props {
   selected: Cuboid;
